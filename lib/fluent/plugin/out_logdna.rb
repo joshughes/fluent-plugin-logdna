b36a0c3ef3b360b3c72cbbd964bf2d890808d081
Joseph Hughes <jjhughes57@gmail.com>
2017-09-14 21:00:09 -0400

    add some config

--- lib/fluent/plugin/out_logdna.rb
+++ lib/fluent/plugin/out_logdna.rb
@@ -1,4 +1,5 @@
 require 'fluent/output'
+require 'oj'
 
 module Fluent
   class LogDNAOutput < Fluent::BufferedOutput
@@ -9,6 +10,8 @@ module Fluent
 
     config_param :api_key, :string, secret: true
     config_param :hostname, :string
+    config_param :hostname_record_key, :string, default: nil
+    config_param :app_record_key, :string, default: nil
     config_param :mac, :string, default: nil
     config_param :ip, :string, default: nil
     config_param :app, :string, default: nil
@@ -39,7 +42,7 @@ module Fluent
 
     def write(chunk)
       body = chunk_to_body(chunk)
-      response = send_request(body)
+      response = send_request(body, host)
       raise 'Encountered server error' if response.code >= 400
       response.flush
     end
@@ -48,13 +51,14 @@ module Fluent
 
     def chunk_to_body(chunk)
       data = []
+      host = ''
 
       chunk.msgpack_each do |(tag, time, record)|
-        line = gather_line_data(tag, time, record)
+        line, host = gather_line_data(tag, time, record)
         data << line unless line[:line].empty?
       end
 
-      { lines: data }
+      { lines: data }, host
     end
 
     def gather_line_data(tag, time, record)
@@ -63,15 +67,28 @@ module Fluent
         timestamp: time,
         line: record['message'] || record.to_json
       }
-      line[:app] = record['_app'] || record['app']
-      line[:app] ||= @app if @app
+      
+      if @app_record_key && record.has_key?(@app_record_key)
+        line[:app] = record[@app_record_key]
+      else
+        line[:app] = record['_app'] || record['app']
+        line[:app] ||= @app if @app
+      end
+      
       line.delete(:app) if line[:app].nil?
-      line
+      
+      if @hostname_record_key && record.has_key?(@hostname_record_key)
+        host = record[@hostname_record_key]
+      else
+        host = @host
+      end
+      
+      line, host
     end
 
-    def send_request(body)
+    def send_request(body, host)
       now = Time.now.to_i
-      url = "/logs/ingest?hostname=#{@host}&mac=#{@mac}&ip=#{@ip}&now=#{now}"
+      url = "/logs/ingest?hostname=#{host}&mac=#{@mac}&ip=#{@ip}&now=#{now}"
       @ingester.headers('apikey' => @api_key,
                         'content-type' => 'application/json')
                .post(url, json: body)
